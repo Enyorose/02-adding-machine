565cfde5b0d508795ab92f7acfab9005634e0429
Jessica.W <jessica.watson1@live.com>
2021-10-13 10:44:18 -0600

    making form more accessable

--- adding-machine.js
+++ adding-machine.js
@@ -1,9 +1,13 @@
 const form = document.querySelector('form')
 
 const handleSubmit = function(event){
+  //stop form from submitting and refreshing the page
   event.preventDefault();
 
+  //calculate total
   const total = Number(form.first.value) + Number (form.second.value);
+
+  //log total to output
   form.total.value = total;
 
   console.log(total);
